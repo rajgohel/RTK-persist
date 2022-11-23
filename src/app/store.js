40c930bfc00600af0b07dc1ce57ac05a73b11dfb
Raj Gohel <rajgohel0001@gmail.com>
2022-11-23 23:14:24 +0530

    RTK with redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import storageSession from 'redux-persist/lib/storage/session'
+import { PersistGate } from 'redux-persist/integration/react'
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage: storageSession,
+}
+
+const rootReducer = combineReducers({
+  counter: counterReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
